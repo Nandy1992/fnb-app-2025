ee174f80904c4fa79f18599aac6411629254036b
Nandy1992 <nandykumalo@gmail.com>
2025-07-18 07:59:19 +0200

    shopping cart
    learning

---
+++ shopping_cart.py
@@ -0,0 +1,17 @@
+
+#Create a shopping cart program that will continuously ask for user for a food product and the price of that product
+#Have an exit clause if the user wishes to stop adding more things to their cart
+#At the end show the food items and the total cost to the user
+
+foods=[]
+prices=[]
+total=0
+
+while True:
+    food= input("Enter a food to buy or press q to quit")
+    if food =='q'
+    break
+else:
+    price=float(input(f"Enter the price of the {food}:R"))
+    foods.append(food)
+    prices.append(price)
